a87928aeea11caf075a9e7eeb6c19b119b51fdd1
BoyChai <1972567225@qq.com>
2023-11-24 20:26:36 +0800

    Add Code ASCII,Example

---
+++ code/ascii.py
@@ -0,0 +1,12 @@
+'''
+ASCII码
+利用ord()函数可以返回某个字符串对应的ascii(10进制)
+利用chat()函数可以返回某个十进制数所对应的ASCII码字符串。
+'''
+# 字符转ascii
+print(ord('a'))
+print(ord('A'))
+# ascii转换字符
+print(chr(97))
+print(chr(65))
+print(chr(65))
